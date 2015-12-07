cc6c1157f9a648b4b044e3c07001828e8373601f
Ernesto Pacheco <erpachecomo@ittepic.edu.mx>
2015-12-07 14:30:42 -0700

    Archivos para autentificación de usuarios

--- models/User.php
+++ models/User.php
@@ -5,34 +5,76 @@ namespace app\models;
 class User extends \yii\base\Object implements \yii\web\IdentityInterface
 {
     public $id;
+    public $email;
+    public $name;
+    public $lastname;
+    public $activate;
     public $username;
     public $password;
     public $authKey;
     public $accessToken;
+    public $role;
 
     private static $users = [
-        '100' => [
-            'id' => '100',
+        'admin' => [
             'username' => 'admin',
             'password' => 'admin',
             'authKey' => 'test100key',
             'accessToken' => '100-token',
+            'role' => 'admin',
         ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
+        'estudiante' => [
+            'username' => 'estudiante',
+            'password' => 'estudiante',
             'authKey' => 'test101key',
             'accessToken' => '101-token',
+            'role' => 'estudiante',
+        ],
+        'maestro' => [
+            'username' => 'maestro',
+            'password' => 'maestro',
+            'authKey' => 'test102key',
+            'accessToken' => '102-token',
+            'role' => 'maestro',
         ],
     ];
+
+    public static function isUserAdmin($username)
+{
+   if (Users::findOne(['username' => $username, 'activate' => '1', 'role' => 'admin'])){
+    return true;
+   } else {
+    return false;
+   }
+}
+    public static function isUserEstudiante($username)
+{
+   if (Users::findOne(['username' => $username, 'activate' => '1', 'role' => 'estudiante'])){
+    return true;
+   } else {
+    return false;
+   }
+}
+    public static function isUserMaestro($username)
+{
+   if (Users::findOne(['username' => $username, 'activate' => '1', 'role' => 'maestro'])){
+    return true;
+   } else {
+    return false;
+   }
+}
 
     /**
      * @inheritdoc
      */
-    public static function findIdentity($id)
+
+    public static function findIdentity($username)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        $user = Users::find()
+                ->where("activate=:activate", [":activate" => 1])
+                ->andWhere("username=:username", ["username" => $username])
+                ->one();
+        return isset($user) ? new static($user) : null;
     }
 
     /**
@@ -40,13 +82,17 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
+                $users = Users::find()
+                ->where("activate=:activate", [":activate" => 1])
+                ->andWhere("accessToken=:accessToken", [":accessToken" => $token])
+                ->all();
+        foreach ($users as $user) {
+            if ($user->accessToken === $token) {
                 return new static($user);
             }
         }
-
         return null;
+
     }
 
     /**
@@ -57,12 +103,15 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
      */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
+        $users = Users::find()
+                ->where("activate=:activate", ["activate" => 1])
+                ->andWhere("username=:username", [":username" => $username])
+                ->all();
+        foreach ($users as $user) {
+            if (strcasecmp($user->username, $username) === 0) {
                 return new static($user);
             }
         }
-
         return null;
     }
 
@@ -73,6 +122,7 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
     {
         return $this->id;
     }
+    
 
     /**
      * @inheritdoc
