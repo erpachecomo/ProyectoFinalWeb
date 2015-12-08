87b0452b7e2c6cf314468c55e28d4dd95be5a21c
Juan Jose Nolasco <jujonolascome@ittepic.edu.mx>
2015-12-07 19:39:53 -0700

    agregue botones modifique .css

--- views/site/admin.php
+++ views/site/admin.php
@@ -4,18 +4,36 @@
 
 use yii\helpers\Html;
 
-$this->title = 'Admin';
+$this->title = 'Administrador';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-admin">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-      Portal de administrador
-    </p>
-    <h2>Administrador</h2>
+    
+  
     <ul>
     		
     </ul>
+    <br>
+
+    <div class="boton-tareas">
+    <p><a class="btn btn-lg btn-primary" href="http://localhost/index.php?r=site%2Flogin">Tareas</a>
+    </div>
+
+    <div class="boton-curso">
+    <a class="btn btn-lg btn-primary" href="http://localhost/index.php?r=site%2Flogin">Cursos</a>
+    </div>
+
+    <div class="boton-imparte">
+    <a class="btn btn-lg btn-primary" href="http://localhost/index.php?r=site%2Flogin">Imparte Curso</a>
+    </div>
+
+    <div class="boton-Alumno">
+    <a class="btn btn-lg btn-primary" href="http://localhost/index.php?r=site%2Flogin">Alumno Curso</a>
+    </div>
+    
+    </p>
+   
 
 </div>
